65c6e01daf5250a1e10a06cc00a2ca289da41fa3
Mukul Rana <56799682+mukul-rana@users.noreply.github.com>
2022-08-27 17:44:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("Change 3");
-    System.out.println("Change 4");
    }
 }
